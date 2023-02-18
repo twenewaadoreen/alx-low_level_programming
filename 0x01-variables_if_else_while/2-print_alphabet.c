3f59e9c85d7645c7e614767d43f0461de0241fd9
twenewaadoreen <twenewaadoreen@gmail.com>
2023-02-17 17:40:36 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower case
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchat('\n');
+	return (0);
+}
