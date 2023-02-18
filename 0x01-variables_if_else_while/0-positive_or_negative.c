86b7257e5c7f3246379c62886c18177cbad1bdb6
twenewaadoreen <twenewaadoreen@gmail.com>
2023-02-17 17:29:57 -0800

    task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-		printf("%d is zero\n" n);
+		printf("%d is zero\n", n);
 	}
 	else if (n > 0)
 	{
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else
 	{
-		printf"%d is negative\n" n);
+		printf"%d is negative\n", n);
 	}
 	return (0);
 }
