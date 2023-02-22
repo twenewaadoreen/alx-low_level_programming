cbf6edaeff672e6b4e7a86702fd981286825f70f
twenewaadoreen <twenewaadoreen@gmail.com>
2023-02-21 17:43:04 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - contains the absolute value of an integer
+ * @c: The number to be computed
+ * Return: Avsolute value of a number or zero
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
