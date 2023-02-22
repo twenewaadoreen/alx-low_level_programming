7440fb0e2b84e66cc9022757e61561f1c41b3b55
twenewaadoreen <twenewaadoreen@gmail.com>
2023-02-21 19:05:24 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: the first number
+ * @n2: the second number
+ * Return: interger value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
