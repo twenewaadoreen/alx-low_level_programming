26b3fffd884f74218a58c403fe8bfd7cce4ecc7c
twenewaadoreen <twenewaadoreen@gmail.com>
2023-04-10 12:12:08 -0700

    tasks

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary value of a dec
+ * @n: number in dec form
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
