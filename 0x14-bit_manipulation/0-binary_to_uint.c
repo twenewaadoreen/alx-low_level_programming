a30965e31d2b7dbd19029bd77695f171999c200f
twenewaadoreen <twenewaadoreen@gmail.com>
2023-05-04 15:37:45 -0700

    task1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,16 +6,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int j;
-        unsigned int res = 0;
+	int j;
+	unsigned int res = 0;
 
-        if (!b)
-                return (0);
-        for (j = 0; b[j]; j++)
-        {
-                if (b[j] < '0' || b[j] > '1')
-                        return (0);
-                res = 2 * res + (b[j] - '0');
-        }
-        return (result);
+	if (!b)
+		return (0);
+	for (j = 0; b[j]; j++)
+	{
+		if (b[j] < '0' || b[j] > '1')
+			return (0);
+		res = 2 * res + (b[j] - '0');
+	}
+	return (result);
 }
