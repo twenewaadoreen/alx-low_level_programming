d3aca7b5acb1c822ca3a60317a07969fa36c4d54
twenewaadoreen <twenewaadoreen@gmail.com>
2023-05-04 15:31:02 -0700

    task1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary number to unsigned int
+ * @b: string of binary values
+ * Return: the converted integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+        int j;
+        unsigned int res = 0;
+
+        if (!b)
+                return (0);
+        for (j = 0; b[j]; j++)
+        {
+                if (b[j] < '0' || b[j] > '1')
+                        return (0);
+                res = 2 * res + (b[j] - '0');
+        }
+        return (result);
+}
